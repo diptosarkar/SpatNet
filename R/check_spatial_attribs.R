d18099a5776dd00c3360edf7cbbb969802d69171
diptosarkar <dipto.sarkar@mail.mcgill.ca>
2017-01-23 17:01:46 -0500

    This function has undergone preliminary testing

--- R/check_spatial_attribs.R
+++ R/check_spatial_attribs.R
@@ -4,11 +4,25 @@
 #' @param: igraph object with V(g)$X and V(g)$Y having the X and Y co-ordinates of each node
 #'
 #' @keywords: spatial degree graph
+#' @references
 #' @export
 #' @examples
 #' spat_degree_change_graph()
 
+library(igraph)
 
 check_spatial_attribs<-function(graph){
-
+  if("X" %in% list.vertex.attributes(graph) && "Y" %in% list.vertex.attributes(graph)){
+    if(is.numeric(V(graph)$X) && is.numeric(V(graph)$Y)){
+      return(TRUE)
+    }
+    else{
+      stop("There seems to be a problem with (X,Y) data in node attributes")
+      return(FALSE)
+    }
+  }
+  else{
+    return(FALSE)
+    stop("(X,Y) attribute for nodes is missing")
+  }
 }
