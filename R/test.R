eb4c661d184118d8ba514637aa9ebcada5066ce9
diptosarkar <dipto.sarkar@mail.mcgill.ca>
2017-01-23 13:03:08 -0500

    More package structure

---
+++ R/test.R
@@ -0,0 +1,12 @@
+#' A trial Function
+#'
+#' This function allows you to express your love of Hello World
+#' @param: None
+#' @keywords: None
+#' @export
+#' @examples
+#' trial
+
+cat_function <- function(){
+  print("Hello World")
+}
