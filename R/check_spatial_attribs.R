de109e01ecd5f1e9660b68dc240d35815c97b8ac
diptosarkar <dipto.sarkar@mail.mcgill.ca>
2017-01-24 10:12:55 -0500

    Added functions for: Checking whether nodes have (x,y) attribute Adding dist attribute to edges Spatial Sociogram

--- R/check_spatial_attribs.R
+++ R/check_spatial_attribs.R
@@ -3,17 +3,17 @@
 #' This function checks whether the graph passsed as an argument has X and Y attributes
 #' @param: igraph object with V(g)$X and V(g)$Y having the X and Y co-ordinates of each node
 #'
-#' @keywords: spatial degree graph
+#' @keywords: check attributes
 #' @references
 #' @export
 #' @examples
 #' spat_degree_change_graph()
 
-library(igraph)
+require(igraph)
 
 check_spatial_attribs<-function(graph){
   if("X" %in% list.vertex.attributes(graph) && "Y" %in% list.vertex.attributes(graph)){
-    if(is.numeric(V(graph)$X) && is.numeric(V(graph)$Y)){
+    if(is.numeric(V(graph)$X) && is.numeric(V(graph)$Y)){   #This returns TRUE if one is NA
       return(TRUE)
     }
     else{
---
+++ R/spatial_sociogram.R
@@ -0,0 +1,26 @@
+#' Spatial Sociogram Function
+#'
+#' This function creates a sociogram where the relative position of the nodes with respect to each other are preserved
+#' @param: igraph object with V(g)$X and V(g)$Y having the X and Y co-ordinates of each node
+#' @param: v_color: color of the vertices. Default=gray
+#' @param: v_lab_color: color of the vertex labels. Default=black
+#' @param: e_width: edge widths. Default=2
+#'
+#' @keywords: spatial sociogram
+#' @references
+#' @export
+#' @examples
+#' spatial_sociogram()
+
+require(igraph)
+
+spatial_sociogram<-function(graph, v_color='gray', v_lab_color="black", e_width=2){
+  if(check_spatial_attribs(graph)){
+    ##Spatial plot
+    layout_spatial<-as.matrix(data.table(x=V(graph)$X, y=V(graph)$Y))
+    plot(graph, layout=layout_spatial, vertex.size = 7, vertex.label.cex=1.5, vertex.color=v_color, vertex.label.color=v_lab_color, edge.width=e_width)
+  }
+  else{
+    stop("Either X and Y data for nodes if missing or there is something wrong with the X and Y data")
+  }
+}
