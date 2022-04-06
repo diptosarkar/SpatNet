632a7bab32538e0fa305da409853ad3b9ce34726
Dipto Sarkar <diptosarkar@users.noreply.github.com>
2022-04-06 13:55:23 -0400

    Revert "V1.1"

---
+++ R/add_edge_dist_attrib.R
@@ -0,0 +1,38 @@
+#' Validation Function
+#'
+#' This function adds a dist attribute to edges as a measure of the Euclidean distance of the connection
+#' @param graph igraph object with V(g)$X and V(g)$Y having the X and Y co-ordinates of each node
+#' @import data.table igraph
+#' @keywords: check attributes
+
+
+
+
+
+add_edge_dist_attrib<-function(graph){
+  if(check_spatial_attribs(graph)){
+    #Create distance matrix
+    tbl<-data.table(ID=V(graph)$name, X=V(graph)$X, Y=V(graph)$Y)
+    dist_mat<-dist(tbl[,2:3,with=F], method = "euclidean", upper = T, diag = T)   #This can be sped up with GPU usage
+    dist_mat2<-as.matrix(dist_mat)  #Store it in matrix format for craeting graph later
+    dist_mat2<-apply(dist_mat2, 1:2, function(x){if(x!=0) x else 1})  #Since nodes may be located in the same place, the dist is 0. Convert 0 to 1 so that when it is multiplied with the adjascency matrix in a few steps, you don't get 0s leading to removal of edges.
+    dist_mat<-as.matrix(dist_mat) #Convert atomic vector to matrix
+
+    weighted_adj_mat<-dist_mat2*as_adjacency_matrix(graph, sparse = F)
+    graph<-graph_from_adjacency_matrix(weighted_adj_mat, mode=c("undirected"), diag = F, weighted = T)
+
+    V(graph)$X<-tbl$X #Add X,Y of each node
+    V(graph)$Y<-tbl$Y
+
+    shortest_path_mat<-as.matrix(distances(graph, weights = NA))
+
+    E(graph)$dist<-E(graph)$weight
+    return(E(graph)$dist)
+
+  }
+  else{
+    stop("Either X and Y data for nodes if missing or there is something wrong with the X and Y data")
+  }
+
+}
+
--- R/spat_degree.R
+++ R/spat_degree.R
@@ -1,22 +1,26 @@
-#' @import igraph data.table crayon
+#' Calculate Spatial Degree
+#'
+#' This function allows you to calculate spatial degree of nodes at a particular alpha
+#' @param graph igraph object with V(g)$X and V(g)$Y having the X and Y co-ordinates of each node.
+#' @param alpha specifies alpha value at which degree is to be calculted
+#' @param rank decides whether to return rank of nodes of raw values. Defaults to TRUE meaning ranks are to be calculated
+#' @keywords: spatial clustering
+#' @import data.table igraph
 #' @export
+#' @examples
+#'
 
-spat_degree<-function(graph, alpha = 0.5, alphaWarning, rank=TRUE){
-
-  require(igraph)
-  require(data.table)
-  require(crayon)
-
+spat_degree<-function(graph, alpha, rank=TRUE){
   if(check_spatial_attribs(graph)){
     tbl<-data.table(ID=V(graph)$name, X=V(graph)$X, Y=V(graph)$Y)
     dist_mat<-dist(tbl[,2:3,with=F], method = "euclidean", upper = T, diag = T)  #Can be sped up with GPU
-    dist_mat2<-as.matrix(dist_mat)  #Store it in matrix format for creating a graph later
-    dist_mat2<-apply(dist_mat2, 1:2, function(x){if(x!=0) x else 1})  #Since a multiple of nodes can be in the same spot, the dist is 0. Convert 0 to 1 so that when it is multiplied with the adjacency matrix in a few steps, the output doesnt't lead to get 0s to removal of edges.
+    dist_mat2<-as.matrix(dist_mat)  #Store it in matrix format for craeting graph later
+    dist_mat2<-apply(dist_mat2, 1:2, function(x){if(x!=0) x else 1})  #Since a multiple nodes can be in the same spot, the dist is 0. Convert 0 to 1 so that when it is multiplied with the adjascency matrix in a few steps, you don't get 0s leading to removal of edges.
     weighted_adj_mat<-dist_mat2*as_adjacency_matrix(graph, sparse = F)
 
     deg_dt<-data.table(vertex_name=V(graph)$name)
 
-    deg_dt$degree_benefit<-apply(weighted_adj_mat, 1, sum)   #Weighted adjacency mat has benefits
+    deg_dt$degree_benefit<-apply(weighted_adj_mat, 1, sum)   #weighted adj mat has benefits
     #deg_dt$degree_benefit<-deg_dt$degree_benefit/1000 #In Km
 
     #Normalize between 0-1
@@ -33,16 +37,13 @@ spat_degree<-function(graph, alpha = 0.5, alphaWarning, rank=TRUE){
     V(graph)$deg_stren<-deg_dt$degree_benefit
 
     rm(deg_dt)
-
-    alphaWarning= cat(crayon::green$bold("Default alpha is set at 0.5\n"))
-
     if(alpha<0.0 | alpha>1.0)
     {
       stop("The value of alpha must be between [0,1]")
     }
     V(graph)$spat_deg<-alpha * (V(graph)$deg_cost) + (1-alpha) * (V(graph)$deg_stren)
     if(!rank){
-      return(V(graph)$spat_deg) #Return raw values
+      return(V(graph)$spat_deg) #return raw values
     }
     if(rank)
     {
@@ -51,7 +52,7 @@ spat_degree<-function(graph, alpha = 0.5, alphaWarning, rank=TRUE){
 
   }
   else{
-    stop("Either (X,Y) data for nodes is missing, or there is an issue with the (X,Y) data")
+    stop("Either X and Y data for nodes if missing or there is something wrong with the X and Y data")
   }
 
 }
